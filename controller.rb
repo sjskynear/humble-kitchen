e21f650b209e63496f2e15afd01b66e1aba092d0
sham skynear <sjskynear@gmail.com>
2013-07-23 12:04:24 -0600

    edits to each erg file  (6)

---
+++ controller.rb
@@ -0,0 +1,29 @@
+require 'sinatra'
+
+# TODO: add your GET handlers here
+
+get '/page1' do
+  halt erb(:page1)
+end
+
+get '/page2' do
+  halt erb(:page2)
+end
+
+get '/page3' do
+  halt erb(:page3)
+end
+
+get '/page4' do
+  halt erb(:page4)  
+end
+
+get '/page5' do
+  halt erb(:page5)
+end
+
+get '/page6' do
+  halt erb(:page6)
+end
+
+
